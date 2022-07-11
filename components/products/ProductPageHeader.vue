2bece32c6bf136949d6607cda06b735e02001793
Squanbri <62174248+Squanbri@users.noreply.github.com>
2022-07-11 03:23:00 +0300

    Added ProductPageHeader

---
+++ components/products/ProductPageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="$style.wrapper">
+    <h3 :class="$style.header">Добавление товара</h3>
+
+    <my-select
+      :options="['go', 'python', 'rust', 'javascript']"
+      :default="'По умолчанию'"
+    />
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style module lang="scss">
+  .wrapper {
+    display: flex;
+    justify-content: space-between
+  }
+
+  .header {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 35px;
+    margin: 0;
+  }
+</style>
