b2ad0a1728c3d060dbacc8037fb8e597d3f3eb59
Squanbri <62174248+Squanbri@users.noreply.github.com>
2022-07-11 13:24:44 +0300

    Added sorting

--- components/products/ProductPageHeader.vue
+++ components/products/ProductPageHeader.vue
@@ -3,14 +3,34 @@
     <h3 :class="$style.header">Добавление товара</h3>
 
     <my-select
-      :options="['go', 'python', 'rust', 'javascript']"
+      :options="['По возрастанию цены', 'По убыванию цены', 'По имени']"
       :default="'По умолчанию'"
+      @change="onSelectSort"
     />
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 
+export default {
+  methods: {
+    onSelectSort(option) {
+      const sorts = {
+        'По возрастанию цены': this.sortByPriceAsc,
+        'По убыванию цены': this.sortByPriceDesc,
+        'По имени': this.sortByName
+      }
+
+      sorts[option]?.()
+    },
+    ...mapMutations({
+      sortByPriceAsc: 'products/sortByPriceAsc',
+      sortByPriceDesc: 'products/sortByPriceDesc',
+      sortByName: 'products/sortByName'
+    })
+  }
+}
 </script>
 
 <style module lang="scss">
