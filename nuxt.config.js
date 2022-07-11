4aa35cbc51fec8b6226c70fe47820d794ccd8aa8
Squanbri <62174248+Squanbri@users.noreply.github.com>
2022-07-11 13:17:54 +0300

    Added validation on products form

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vee-validate.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -49,11 +50,17 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['vee-validate'],
     loaders: {
       cssModules: {
         modules: {
           localIdentName: '[local]_[hash:base64:8]'
         }
+      }
+    },
+    postcss: {
+      plugins: {
+        'postcss-import': true
       }
     }
   }
